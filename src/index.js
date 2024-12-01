a799b573354dc19bbd8a034b722128e3a892fc3c
Rachel Michelson <rachelmichelson@Rachels-MacBook-Air-6.local>
2024-12-01 11:20:25 -0500

    learning

--- src/index.js
+++ src/index.js
@@ -11,6 +11,23 @@ root.render(
   </React.StrictMode>
 );
 
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+function hi() {
+  return (
+    <div>
+      <Welcome name="Sara" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" />
+    </div>
+  );
+}
+
+
+export default hi;
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
